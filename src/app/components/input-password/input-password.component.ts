e81d6802c02f7752aa513766202fcdaf91c3629e
Roman Tkachuk <tkachukrr@gmail.com>
2023-11-09 10:01:49 +0100

    Added basic password input structure

--- src/app/components/input-password/input-password.component.ts
+++ src/app/components/input-password/input-password.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-input-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './input-password.component.html',
   styleUrl: './input-password.component.scss'
 })
 export class InputPasswordComponent {
-
+  inputPassword: FormControl = new FormControl('')
 }
