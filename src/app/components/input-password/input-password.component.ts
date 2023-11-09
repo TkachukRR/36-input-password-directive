11b7fe2da4bf585c111f6aae337d78e47a33b5d1
Roman Tkachuk <tkachukrr@gmail.com>
2023-11-09 15:40:39 +0100

    Moved password handler logic to directive and used element attributes

--- src/app/components/input-password/input-password.component.ts
+++ src/app/components/input-password/input-password.component.ts
@@ -1,56 +1,15 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
-import { PasswordState } from "../../interfaces/password-state";
-import { InputStatus } from "../../enums/input-status";
-import { PasswordStrength } from "../../enums/password-strength";
-import { PasswordPatterns } from "./password-patterns";
+import { PasswordStateDirective } from "../../directives/password-state.directive";
 
 @Component({
   selector: 'app-input-password',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, PasswordStateDirective],
   templateUrl: './input-password.component.html',
   styleUrl: './input-password.component.scss'
 })
-export class InputPasswordComponent implements OnDestroy{
+export class InputPasswordComponent {
   inputPassword: FormControl = new FormControl('')
-  passwordState: PasswordState = { status: InputStatus.empty }
-
-  valueSub = this.inputPassword.valueChanges.subscribe(val => this.checkPasswordState(val))
-
-  ngOnDestroy() {
-    this.valueSub.unsubscribe()
-  }
-
-  checkPasswordState(password: string){
-    if (this.passwordState.strengthStatus) delete this.passwordState.strengthStatus
-
-    switch (true) {
-      case password.length === 0:
-        this.passwordState.status = InputStatus.empty;
-        break;
-      case password.length < 8:
-        this.passwordState.status = InputStatus.short;
-        break;
-      default:
-        this.passwordState.status = InputStatus.strength;
-        this.setStrengthType(password)
-        break;
-    }
-  }
-
-  setStrengthType(password: string){
-    const onlyLetters = PasswordPatterns['letters'].test(password);
-    const onlyDigits = PasswordPatterns['digits'].test(password);
-    const onlySymbols = PasswordPatterns['symbols'].test(password)    ;
-    const onlyLettersAndSymbols = PasswordPatterns['lettersAndSymbols'].test(password);
-    const onlyDigitsAndLetters = PasswordPatterns['digitsAndLetters'].test(password);
-    const onlySymbolsAndDigits = PasswordPatterns['symbolsAndDigits'].test(password);
-    const onlyLettersAndDigitsAndSymbols = PasswordPatterns['lettersAndDigitsAndSymbols'].test(password);
-
-    if (onlyLetters || onlyDigits || onlySymbols) this.passwordState.strengthStatus = PasswordStrength.easy
-    if (onlyLettersAndSymbols || onlyDigitsAndLetters || onlySymbolsAndDigits) this.passwordState.strengthStatus = PasswordStrength.medium
-    if (onlyLettersAndDigitsAndSymbols) this.passwordState.strengthStatus = PasswordStrength.strong
-  }
 }
--- src/app/interfaces/password-state.ts
+++
@@ -1,7 +0,0 @@
-import { InputStatus } from "../enums/input-status";
-import { PasswordStrength } from "../enums/password-strength";
-
-export interface PasswordState{
-  status: InputStatus;
-  strengthStatus?: PasswordStrength
-}
