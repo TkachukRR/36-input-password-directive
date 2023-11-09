5ac44a7717d4cc5c8d8298842913477a433b5668
Roman Tkachuk <tkachukrr@gmail.com>
2023-11-09 09:50:57 +0100

    Generated input-password component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InputPasswordComponent } from "./components/input-password/input-password.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InputPasswordComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/components/input-password/input-password.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input-password',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-password.component.html',
+  styleUrl: './input-password.component.scss'
+})
+export class InputPasswordComponent {
+
+}
