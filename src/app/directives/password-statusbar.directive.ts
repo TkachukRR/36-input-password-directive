98eb10ed5bdcf95fbbd5baf5b6bbb6ea57fdd4cd
Roman Tkachuk <tkachukrr@gmail.com>
2023-11-10 13:07:16 +0100

    Return inner content in PasswordStatusbarDirective

--- src/app/directives/password-statusbar.directive.ts
+++ src/app/directives/password-statusbar.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import {Directive, inject, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[appPasswordStatusbar]',
   standalone: true
 })
-export class PasswordStatusbarDirective {
+export class PasswordStatusbarDirective implements OnInit{
+  private viewContainer = inject(ViewContainerRef);
+  private templateRef = inject(TemplateRef);
 
-
+  ngOnInit(): void {
+    this.viewContainer.createEmbeddedView(this.templateRef)
+  }
 }
