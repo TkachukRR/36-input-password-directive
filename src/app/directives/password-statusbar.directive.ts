8aef83716ad2e39317bdf4a4297a17fd46adcd4e
Roman Tkachuk <tkachukrr@gmail.com>
2023-11-10 14:12:30 +0100

    Added input wrapper, added directive to element, added password status bar

--- src/app/directives/password-statusbar.directive.ts
+++ src/app/directives/password-statusbar.directive.ts
@@ -26,5 +26,28 @@ export class PasswordStatusbarDirective implements OnInit{
       const value = (event.target as HTMLInputElement).value;
       console.log('value:', value);
     });
+    const inputPasswordWrapper = inputPassword.parentElement
+    this.renderer.setStyle(inputPasswordWrapper, 'display','inline-block')
+
+    const statusbar: HTMLDivElement = this.createStatusbar()
+    this.renderer.appendChild(inputPassword.parentNode, statusbar)
+  }
+
+  createStatusbar(): HTMLDivElement {
+    const divBar: HTMLDivElement = this.renderer.createElement('div')
+
+    const firstInsideBlock = this.renderer.createElement('div')
+    const secondInsideBlock = this.renderer.createElement('div')
+    const thirdFirstInsideBlock = this.renderer.createElement('div')
+
+    this.renderer.addClass(divBar, 'statusbar')
+    this.renderer.addClass(firstInsideBlock, 'statusbar__item')
+    this.renderer.addClass(secondInsideBlock, 'statusbar__item')
+    this.renderer.addClass(thirdFirstInsideBlock, 'statusbar__item')
+
+    this.renderer.appendChild(divBar, firstInsideBlock);
+    this.renderer.appendChild(divBar, secondInsideBlock);
+    this.renderer.appendChild(divBar, thirdFirstInsideBlock);
+    return divBar
   }
 }
