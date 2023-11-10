2b25bb72d8ca4b96ddd6bf955413043d47d4958e
Roman Tkachuk <tkachukrr@gmail.com>
2023-11-10 13:19:06 +0100

    Added ability listen input value in PasswordStatusbarDirective

--- src/app/directives/password-statusbar.directive.ts
+++ src/app/directives/password-statusbar.directive.ts
@@ -1,4 +1,12 @@
-import {Directive, inject, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  inject,
+  OnInit,
+  Renderer2,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
 
 @Directive({
   selector: '[appPasswordStatusbar]',
@@ -7,8 +15,16 @@ import {Directive, inject, OnInit, TemplateRef, ViewContainerRef} from '@angular
 export class PasswordStatusbarDirective implements OnInit{
   private viewContainer = inject(ViewContainerRef);
   private templateRef = inject(TemplateRef);
+  private elementRef = inject(ElementRef);
+  private renderer = inject(Renderer2)
 
   ngOnInit(): void {
     this.viewContainer.createEmbeddedView(this.templateRef)
+    const inputPassword: HTMLInputElement = this.renderer.selectRootElement('input[type="password"]', this.elementRef.nativeElement)
+
+    this.renderer.listen(inputPassword,'input', (event) => {
+      const value = (event.target as HTMLInputElement).value;
+      console.log('value:', value);
+    });
   }
 }
